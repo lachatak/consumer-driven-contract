c587f3bd909477e3633f44dd15b6e60a9693b7bf
klachata <klachata@gumtree.com>
2016-07-23 22:32:41 +0100

    - update pact maven plugin version to use new version which has the merge function from me - remove scalatest pact dsl as it moves to jvm pact official repo

--- time/src/test/scala/org/kaloz/cdc/time/TimeStatefulSpec.scala
+++ time/src/test/scala/org/kaloz/cdc/time/TimeStatefulSpec.scala
@@ -2,44 +2,27 @@ package org.kaloz.cdc.time
 
 import java.net.URL
 
-import org.kaloz.pact.provider.scalatest.{PactProviderRestartDslSpec, PactProviderStatefulDslSpec}
+import au.com.dius.pact.provider.scalatest.{PactProviderStatefulDslSpec, ServerStarter}
 
 class TimeStatefulSpec extends PactProviderStatefulDslSpec("time") {
 
-  var server: ServerImpl = _
+  lazy val serverStarter: ServerStarter = new ServerStarter {
 
-  override def startServer(): URL = {
-    println("Starting server...")
-    server = new ServerImpl()
-    server.startServer()
-    server.url()
+    var server: ServerImpl = _
+
+    override def startServer(): URL = {
+      println("Starting server...")
+      server = new ServerImpl()
+      server.startServer()
+      server.url()
+    }
+
+    override def initState(state: String): Unit = if (state.contains("01-07-2017")) server.date = "01-07-2017" else server.date = "01-07-2016"
+
+    override def stopServer(): Unit = {
+      println("Stopping server...")
+      server.stopServer()
+    }
+
   }
-
-  override def initState(state: String): Unit = if (state.contains("01-07-2017")) server.date = "01-07-2017" else server.date = "01-07-2016"
-
-  override def stopServer(): Unit = {
-    println("Stopping server...")
-    server.stopServer()
-  }
-
-}
-
-class TimeRestartSpec extends PactProviderRestartDslSpec("time") {
-
-  var server: ServerImpl = _
-
-  override def startServer(): URL = {
-    println("Starting server...")
-    server = new ServerImpl()
-    server.startServer()
-    server.url()
-  }
-
-  override def initState(state: String): Unit = if (state.contains("01-07-2017")) server.date = "01-07-2017" else server.date = "01-07-2016"
-
-  override def stopServer(): Unit = {
-    println("Stopping server...")
-    server.stopServer()
-  }
-
 }
